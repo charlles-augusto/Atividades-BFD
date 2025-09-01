06dd4d6d4be46b8e2e592b74d0ba897c0c061c50
charlles-augusto <225270002JO@aluno.ucam.edu.br>
2025-09-01 16:35:21 -0300

    .

--- atividades/lista_laco.py
+++ atividades/lista_laco.py
@@ -1,13 +1,9 @@
-# Get input from user
 n = int(input("Digite um número N: "))
 
-# Create empty list to store even numbers
 even_numbers = []
 
-# Loop from 1 to N and add even numbers to list
 for i in range(1, n + 1):
     if i % 2 == 0:
         even_numbers.append(i)
 
-# Print the list of even numbers
 print(f"Números pares de 1 até {n}: {even_numbers}")
